8fadd6563a79a94a44f09ede0989e0d2605c3c94
Tri Nguyen <nguyenmtri11@gmail.com>
2024-09-14 01:26:28 +0700

    Update types

--- redbelly-wallet-js.d.ts
+++ redbelly-wallet-js.d.ts
@@ -1,1 +1,19 @@
-declare module 'redbelly-wallet-js';
+declare module 'redbelly-wallet-js' {
+  export default class RedbellyWallet {
+    constructor(rpcUrl: string);
+    createWalletFromMnemonic(mnemonic: string): Promise<{
+      privateKey: string;
+      publicKey: string;
+      address: string;
+      mnemonic: string;
+      balance: string;
+    }>;
+    isMetaMaskAvailable(): Promise<boolean>;
+    connectMetaMask(): Promise<string>;
+    getBalance(address: string): Promise<string>;
+    sendTransactionWithMetaMask(to: string, amount: number): Promise<string>;
+    sendTransaction(privateKey: string, to: string, amount: number): Promise<any>;
+    isRedbellyNetworkConfigured(): Promise<boolean>;
+    addRedbellyNetwork(): Promise<void>;
+  }
+}
